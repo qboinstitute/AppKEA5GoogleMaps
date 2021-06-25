6dcae3c5ad5e23318ae36ec02548caa961730731
LuisAngel <luisangel.salvat@gmail.com>
2021-06-25 11:34:38 -0500

    Actualización 5, agregar polilineas y polígonos.

--- app/src/main/java/com/qbo/appkea5googlemaps/MapsActivity.kt
+++ app/src/main/java/com/qbo/appkea5googlemaps/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.qbo.appkea5googlemaps
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,18 +8,18 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import com.google.maps.android.PolyUtil
 import com.qbo.appkea5googlemaps.commom.AppMensaje
 import com.qbo.appkea5googlemaps.commom.TipoMensaje
 import com.qbo.appkea5googlemaps.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
-        GoogleMap.OnMapClickListener {
+        GoogleMap.OnMapClickListener, GoogleMap.OnMarkerDragListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private var lstLatLong = ArrayList<LatLng>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +35,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapClickListener(this)
+        mMap.setOnMarkerDragListener(this)
         // Add a marker in Sydney and move the camera
         val puntoReferencia = LatLng(-12.074566, -77.036039)
         mMap.addMarker(
@@ -45,7 +48,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
             .snippet("Ahora mismo me encuentro aquí"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(puntoReferencia,
             16.0F))
-        mMap.isTrafficEnabled = true
+        //mMap.isTrafficEnabled = true
         //mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
         try{
             mMap.isMyLocationEnabled = true
@@ -53,8 +56,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
             AppMensaje.enviarMensaje(binding.root,
             getString(R.string.errorgps), TipoMensaje.ERROR)
         }
+        agregarPoligono()
+    }
+
+    private fun agregarPoligono(){
+        val poligonoMap = mMap.addPolygon(
+            PolygonOptions()
+                .add(
+                    LatLng(-12.092776,-76.995812),
+                    LatLng(-12.089579,-77.001294),
+                    LatLng(-12.088362,-77.003419),
+                    LatLng(-12.093985,-77.002711)
+                )
+        )
+        poligonoMap.tag = "QBO"
+        var puntoValidar = PolyUtil.containsLocation(
+            LatLng(-12.074566, -77.036039),
+            poligonoMap.points, false
+        )
+        if(puntoValidar){
+            AppMensaje.enviarMensaje(binding.root,
+            getString(R.string.msjpuntodentro),
+            TipoMensaje.EXITO)
+        }else{
+            AppMensaje.enviarMensaje(binding.root,
+                getString(R.string.msjpuntoafuera),
+                TipoMensaje.ERROR)
+        }
 
     }
+
 
     override fun onMapClick(p0: LatLng) {
         mMap.addMarker(
@@ -63,5 +94,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
                 .title("Nuevo marcador")
         )
         mMap.animateCamera(CameraUpdateFactory.newLatLng(p0))
+        lstLatLong.add(p0)
+        val polyLinea = PolylineOptions()
+        polyLinea.color(Color.RED)
+        polyLinea.width(6F)
+        polyLinea.addAll(lstLatLong)
+        mMap.addPolyline(polyLinea)
+    }
+
+    override fun onMarkerDragStart(p0: Marker) {
+        p0.showInfoWindow()
+    }
+
+    override fun onMarkerDrag(p0: Marker) {
+        var posicion = p0.position
+        p0.snippet = posicion.latitude.toString()+" - "+
+                posicion.longitude.toString()
+        p0.showInfoWindow()
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(posicion))
+    }
+
+    override fun onMarkerDragEnd(p0: Marker) {
+        p0.title = "Nueva posición"
+        p0.showInfoWindow()
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(p0.position))
     }
 }
